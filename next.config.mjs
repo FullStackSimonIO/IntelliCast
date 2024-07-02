5f3ad5679cb932cde3ffd58490274cd3ca22caf4
FullStackSimon <fullstacksimon@gmail.com>
2024-07-02 17:54:49 +0200

    added thumbnail generator

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,23 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
       },
       {
-        protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        protocol: "https",
+        hostname: "sleek-capybara-771.convex.cloud",
       },
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com'
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "cool-malamute-536.convex.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
