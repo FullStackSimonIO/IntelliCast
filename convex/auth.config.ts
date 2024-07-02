73d85b155b92bf1a4ba757fd862fb98eb66e1cdf
FullStackSimon <fullstacksimon@gmail.com>
2024-07-02 17:20:25 +0200

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://ace-squid-28.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://ace-squid-28.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
